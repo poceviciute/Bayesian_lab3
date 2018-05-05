eaac9be4d566e2fba24e72b66e7abff234320088
Fanny Karelius <fanny.karelius@telia.com>
2018-05-05 14:02:03 +0200

    working stan model

--- lab3q2.R
+++ lab3q2.R
@@ -5,11 +5,11 @@ library(rstan)
 ar_process = '
 data {
   int<lower=0> iter;
-}
-parameters {
   real mu;
   real<lower=0> sigma2;
   real<lower=-1, upper=1> phi;
+}
+parameters {
   real x[iter];
 }
 model {
@@ -21,14 +21,14 @@ model {
 burnin <- 100
 niter <- 200
 mu <- 10
-init_para <- list(list(sigma2=2, phi=0.5, mu=10, x=c(mu,rep(0,niter-1))))
+init_para <- list(list(x=c(mu,rep(0,niter-1))))
 #needs to be a list of a list since each chain needs its own list
 fit1<-stan(model_code=ar_process,
-           data=list(iter=niter),
+           data=list(iter=niter,sigma2=2, phi=0.5, mu=10),
            init = init_para,
            warmup=burnin,
            iter=niter,
            chains = 1)
 print(fit1)
-fit1_postmean <- get_posterior_mean(fit1)[5:niter]
+fit1_postmean <- get_posterior_mean(fit1)[burnin:niter]
 plot(fit1_postmean, type="l")
